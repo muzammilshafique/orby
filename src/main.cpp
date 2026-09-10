ead994e7c72ec3ea61ad60bde7aa801fa762f223
muzammil <muzammilshafique.mms@gmail.com>
2026-09-10 21:24:51 +0500

    feat(tray): integrate TrayManager and QApplication in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,32 @@
 #include "DiscordApi.h"
 #include "ProcessSpoofer.h"
-#include <QGuiApplication>
+#include "TrayManager.h"
+
+#include <QApplication>
+#include <QIcon>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QQuickStyle>
 
 int main(int argc, char *argv[]) {
-  QGuiApplication app(argc, argv);
+  QApplication app(argc, argv);
   QQuickStyle::setStyle("Basic");
   app.setOrganizationName("orby");
   app.setOrganizationDomain("orby.org");
   app.setApplicationName("Orby");
+  app.setWindowIcon(QIcon(":/icons/orby.png"));
+
+  // Keep application running in tray when the window is closed
+  app.setQuitOnLastWindowClosed(false);
 
   qmlRegisterType<ProcessSpoofer>("Orby", 1, 0, "ProcessSpoofer");
   qmlRegisterType<DiscordApi>("Orby", 1, 0, "DiscordApi");
 
+  TrayManager trayManager;
+
   QQmlApplicationEngine engine;
+  engine.rootContext()->setContextProperty("trayManager", &trayManager);
+
   const QUrl url(QStringLiteral("qrc:/Orby/qml/Main.qml"));
   QObject::connect(
       &engine, &QQmlApplicationEngine::objectCreationFailed, &app,
